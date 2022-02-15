8298a82e8c4be12822aa1454d408df296a497fb6
Nariman Rajabi <nariman.rajabi@gmail.com>
2022-02-15 22:15:37 +0330

    added starting script for payments

--- payments/src/index.ts
+++ payments/src/index.ts
@@ -5,6 +5,7 @@ import { OrderCancelledListener } from './events/listeners/order-cancelled-liste
 import { OrderCreatedListener } from './events/listeners/order-created-listener';
 
 const start = async () => {
+    console.log('Starting...');
     // check env variable is defined
     if (!process.env.JWT_KEY) {
         throw new Error('JWT_KEY must be defined');
